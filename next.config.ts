fe4fdde9a58fedc60ca9cfd9da604cef7ca1812b
alasal3 <emohamedalasal@gmail.com>
2025-12-20 03:11:58 +0200

    Clear git cache and refresh .gitignore

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
   cleanDistDir: true,
   compress: false,
   poweredByHeader: false,
-=======
-  /* config options here */
   images: {
     remotePatterns: [
       {
@@ -24,7 +21,6 @@ const nextConfig: NextConfig = {
     ],
     formats: ['image/avif', 'image/webp'],
   },
->>>>>>> temp-fix
 };
 
 export default nextConfig;
